0c71299562e2d9b7c131868c402380f115fb7ea0
Menghe Lu <lumenghe@gmail.com>
2024-04-01 21:43:13 +0200

    feat(breadth-first-search): simple breadth first search

---
+++ breadth-first-search.py
@@ -0,0 +1,22 @@
+graph = {
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : []
+}
+
+def bfs(visited: list, graph: dict, node: str) -> None:
+    visited = [node]
+    queue = [node]
+    while queue:
+        checking_node = queue.pop(0)
+        print(checking_node, end=" ")
+        for neighbour in graph[checking_node]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+            
+
+bfs(visited=[], graph=graph, node='A')
