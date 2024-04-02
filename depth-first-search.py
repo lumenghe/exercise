39a8d2b7432afd9c5a78caa5edcb979e8d2e56a7
Menghe Lu <lumenghe@gmail.com>
2024-04-02 21:12:36 +0200

    feat(graph): add depth first search

---
+++ depth-first-search.py
@@ -0,0 +1,20 @@
+graph = {
+    'A' : ['B','C'],
+    'B' : ['D', 'E'],
+    'C' : ['F'],
+    'D' : [],
+    'E' : ['F'],
+    'F' : []
+}
+
+
+def dfs(visited: list, graph: dict, node: str) -> None:
+    if node not in visited:
+        visited.append(node)
+        print(f"visiting {node}")
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+
+
+dfs(visited=[], graph=graph, node='A')
