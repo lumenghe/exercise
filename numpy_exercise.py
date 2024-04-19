0fd8f73c07332d3f2e51a9c58dc4bd4d2c99315a
Menghe Lu <lumenghe@gmail.com>
2024-04-19 06:02:36 +0200

    feat(numpy): add numpy execise

--- numpy_exercise.py
+++ numpy_exercise.py
@@ -43,3 +43,40 @@ if __name__ == "__main__":
     X, Y = Z[:, 0], Z[:, 1]
     print(np.sqrt(X**2 + Y**2))
     print(np.arctan2(Y, X))
+
+    Z = np.zeros((10, 10), [("x", float), ("y", float)])
+    print(Z)
+
+    Z["x"], Z["y"] = np.meshgrid(np.linspace(0, 1, 10), np.linspace(0, 1, 10))
+    print(Z["x"], Z["y"], Z)
+    print("---------------------------")
+
+    for dtype in [np.int8, np.int32, np.int64]:
+        print(np.iinfo(dtype).min)
+        print(np.iinfo(dtype).max)
+
+    for dtype in [np.float32, np.float64]:
+        print(np.finfo(dtype).min)
+        print(np.finfo(dtype).max)
+        print(np.finfo(dtype).eps)
+
+    Z = np.zeros(
+        10,
+        [
+            ("position", [("x", float, (1,)), ("y", float, (1,))]),
+            ("color", [("r", float, (1,)), ("g", float, (1,)), ("b", float, (1,))]),
+        ],
+    )
+    print(Z)
+
+    Z = np.random.random((10, 2))
+    X, Y = np.atleast_2d(Z[:, 0]), np.atleast_2d(Z[:, 1])
+    D = np.sqrt((X - X.T) ** 2 + (Y - Y.T) ** 2)
+
+    sigma, mu = 1.0, 0.0
+    G = np.exp(-((D - mu) ** 2 / (2.0 * sigma**2)))
+    print(G)
+
+    Z = np.array([1, 2, 3, 4, 5])
+    Z0 = np.zeros(len(Z) * 4 - 1)
+    Z0[::4] = Z
