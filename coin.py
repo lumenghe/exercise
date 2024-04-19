c25aa9edd98f3ac0f56130e2896722aa1fa6eedb
Menghe Lu <lumenghe@gmail.com>
2024-04-19 21:16:05 +0200

    feat(scipy): add scipu

---
+++ coin.py
@@ -0,0 +1,14 @@
+import math
+from scipy.special import comb
+
+
+if __name__ == "__main__":
+    print(math.factorial(6))
+    print(comb(3, 3, exact=True))
+
+    sum = 0
+    for n in range(41):
+        sum += comb(100, n, exact=True)
+
+    print(sum)
+    print(sum / (2**100))
