592b027b3c257d8fc1350b029b5df3029b60ed4d
Menghe Lu <lumenghe@gmail.com>
2024-05-05 10:08:45 +0200

    feat(path_sum): add path sum

--- path_sum.py
+++ path_sum.py
@@ -41,3 +41,23 @@ Constraints:
 
 """
 
+from typing import Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def has_path_sum(root: Optional[TreeNode], target_sum: int) -> bool:
+
+    if not root:
+        return False
+
+    if not root.left and not root.right:
+        return root.val == target_sum
+
+    return has_path_sum(root.left, target_sum - root.val) or has_path_sum(root.right, target_sum - root.val)
