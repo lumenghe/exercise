6c546418694d460df664329f135d4281e9cfb5a3
Menghe Lu <lumenghe@gmail.com>
2024-05-10 13:18:22 +0200

    feat(queue): add queue using two stacks

---
+++ queue_using_two_stacks.py
@@ -0,0 +1,130 @@
+"""
+Queue using Two Stacks
+
+A queue is an abstract data type that maintains the order in which elements were added to it, allowing the oldest elements to be removed from the front and new elements to be added to the rear. This is called a First-In-First-Out (FIFO) data structure because the first element added to the queue (i.e., the one that has been waiting the longest) is always the first one to be removed.
+
+A basic queue has the following operations:
+
+    Enqueue: add a new element to the end of the queue.
+    Dequeue: remove the element from the front of the queue and return it.
+
+In this challenge, you must first implement a queue using two stacks. Then process
+queries, where each query is one of the following
+
+types:
+
+    1 x: Enqueue element 
+
+    into the end of the queue.
+    2: Dequeue the element at the front of the queue.
+    3: Print the element at the front of the queue.
+
+Input Format
+
+The first line contains a single integer,
+, denoting the number of queries.
+Each line of the subsequent lines contains a single query in the form described in the problem statement above. All three queries start with an integer denoting the query , but only query is followed by an additional space-separated value,
+
+, denoting the value to be enqueued.
+
+Constraints
+
+It is guaranteed that a valid answer always exists for each query of type
+
+    .
+
+Output Format
+
+For each query of type
+
+, print the value of the element at the front of the queue on a new line.
+
+Sample Input
+
+STDIN   Function
+-----   --------
+10      q = 10 (number of queries)
+1 42    1st query, enqueue 42
+2       dequeue front element
+1 14    enqueue 42
+3       print the front element
+1 28    enqueue 28
+3       print the front element
+1 60    enqueue 60
+1 78    enqueue 78
+2       dequeue front element
+2       dequeue front element
+
+Sample Output
+
+14
+14
+
+Explanation
+
+Perform the following sequence of actions:
+
+    Enqueue 
+
+;
+.
+Dequeue the value at the head of the queue,
+;
+.
+Enqueue
+;
+.
+Print the value at the head of the queue,
+;
+.
+Enqueue
+;
+.
+Print the value at the head of the queue,
+;
+.
+Enqueue
+;
+.
+Enqueue
+;
+.
+Dequeue the value at the head of the queue,
+;
+.
+Dequeue the value at the head of the queue,
+; .
+"""
+
+
+class Queue:
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def enqueue(self, x):
+        self.s1.append(x)
+
+    def dequeue(self):
+        if len(self.s2) == 0:
+            if len(self.s1) == 0:
+                raise IndexError("empty queue")
+            while len(self.s1) > 0:
+                self.s2.append(self.s1.pop())
+
+        return self.s2.pop()
+
+    def print(self):
+        print(self.s1[0])
+
+
+n = int(input())
+queue = Queue()
+for i in range(n):
+    x = input().split()
+    if x[0] == "1":
+        queue.enqueue(x[1])
+    if x[0] == "2":
+        queue.dequeue()
+    if x[0] == "3":
+        queue.print()
