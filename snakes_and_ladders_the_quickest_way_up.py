2715597fba4b8453e80d6f0356cc0c5ed58f9f09
Menghe Lu <lumenghe@gmail.com>
2024-05-11 14:33:32 +0200

    feat(snake_and_ladders) add snakes and ladders the quickest way up

--- snakes_and_ladders_the_quickest_way_up.py
+++ snakes_and_ladders_the_quickest_way_up.py
@@ -107,3 +107,73 @@ For the second test:
 The player first rolls
 and climbs the ladder to square . Three rolls of get to square . A final roll of lands on the target square in total rolls. 
 """
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'quickestWayUp' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. 2D_INTEGER_ARRAY ladders
+#  2. 2D_INTEGER_ARRAY snakes
+#
+
+
+def quickestWayUp(ladders, snakes):
+    skipstart, skipend = zip(*(snakes + ladders))
+    next_space = [1]
+    visited = set()
+    visited.add(1)
+    rolls = 0
+
+    while 100 not in visited:
+        curr = next_space
+        rolls += 1
+
+        next_space = []
+        for start in curr:  # run once for every current space
+            for roll in range(1, 7):  # run once for every roll
+                end = start + roll
+                if end in skipstart:
+                    end = skipend[skipstart.index(end)]  # follow the snake/ladder if applicable
+                if end not in visited:
+                    next_space.append(end)
+                    visited.add(end)
+        if len(next_space) == 0:
+            rolls = -1
+            break
+    return rolls
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        ladders = []
+
+        for _ in range(n):
+            ladders.append(list(map(int, input().rstrip().split())))
+
+        m = int(input().strip())
+
+        snakes = []
+
+        for _ in range(m):
+            snakes.append(list(map(int, input().rstrip().split())))
+
+        result = quickestWayUp(ladders, snakes)
+
+        fptr.write(str(result) + "\n")
+
+    fptr.close()
