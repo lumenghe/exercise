cb9187fe3c314559884715202ed0279c2e733043
Menghe Lu <lumenghe@gmail.com>
2024-05-13 21:55:08 +0200

    feat(bfs): sum of nodes with even valued grandparent

---
+++ sum_of_nodes_with_even_valued_grandparent.py
@@ -0,0 +1,55 @@
+"""
+Sum of Nodes with Even-Valued Grandparent
+Medium
+Topics
+Companies
+Hint
+
+Given the root of a binary tree, return the sum of values of nodes with an even-valued grandparent. If there are no nodes with an even-valued grandparent, return 0.
+
+A grandparent of a node is the parent of its parent if it exists.
+
+ 
+
+Example 1:
+
+Input: root = [6,7,8,2,7,1,3,9,null,1,4,null,null,null,5]
+Output: 18
+Explanation: The red nodes are the nodes with even-value grandparent while the blue nodes are the even-value grandparents.
+
+Example 2:
+
+Input: root = [1]
+Output: 0
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 104].
+    1 <= Node.val <= 100
+
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumEvenGrandparent(self, root: TreeNode) -> int:
+        total = []
+
+        def dfs(node, parent, garendparent):
+            if node is None:
+                return
+            if garendparent and garendparent.val % 2 == 0:
+                total.append(node.val)
+            dfs(node.left, node, parent)
+            dfs(node.right, node, parent)
+
+        dfs(root, None, None)
+        return sum(total)
