8551f7a9aa619aef96d2616aae55404d3d90833d
Menghe Lu <lumenghe@gmail.com>
2024-05-26 21:26:55 +0200

    feat(dp): longest increasing subsequence

---
+++ longest_increasing_subsequence.py
@@ -0,0 +1,56 @@
+"""
+ Longest Increasing Subsequence
+Solved
+Medium
+Topics
+Companies
+
+Given an integer array nums, return the length of the longest strictly increasing
+subsequence
+.
+
+ 
+
+Example 1:
+
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
+
+Example 2:
+
+Input: nums = [0,1,0,3,2,3]
+Output: 4
+
+Example 3:
+
+Input: nums = [7,7,7,7,7,7,7]
+Output: 1
+
+ 
+
+Constraints:
+
+    1 <= nums.length <= 2500
+    -104 <= nums[i] <= 104
+
+ 
+
+Follow up: Can you come up with an algorithm that runs in O(n log(n)) time complexity?
+"""
+
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if nums == []:
+            return 0
+        dp = [1 for _ in range(len(nums))]
+
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return max(dp)
