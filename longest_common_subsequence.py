56b31296be7c7b2e46324b5abacc1605cd27fc40
Menghe Lu <lumenghe@gmail.com>
2024-05-28 21:01:43 +0200

    feat(dp): longest common subsequence

--- longest_common_subsequence.py
+++ longest_common_subsequence.py
@@ -57,3 +57,27 @@ class Solution:
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
         return dp[len1][len2]
+
+
+class Solution1:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        len1, len2 = len(text1), len(text2)
+        dp = [[0] * (len2) for _ in range(len1)]
+
+        for i in range(len1):
+            for j in range(len2):
+                if text1[i] == text2[j]:
+                    if i > 0 and j > 0:
+                        dp[i][j] = dp[i - 1][j - 1] + 1
+                    else:
+                        dp[i][j] = 1
+                else:
+                    if i > 0 and j > 0:
+                        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+                    elif i > 0:
+                        dp[i][j] = dp[i - 1][j]
+                    elif j > 0:
+                        dp[i][j] = dp[i][j - 1]
+                    else:
+                        dp[i][j] = 0
+        return dp[len1 - 1][len2 - 1]
