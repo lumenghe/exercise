96b88532407b70e0fc17024e97c49da7cb4990ca
Menghe Lu <lumenghe@gmail.com>
2024-06-02 18:04:09 +0200

    feat(single_number): single number

--- climbing_stairs.py
+++ climbing_stairs.py
@@ -29,7 +29,15 @@ Explanation: There are three ways to climb to the top.
 3. 2 steps + 1 step
 
 """
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n+1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
 
+        return dp[n]
 
 def climb_staires(n: int) -> int:
     if n == 1 or n == 0:
--- sliding_window_median.py
+++ sliding_window_median.py
@@ -90,3 +90,12 @@ class Solution:
         res.append(float(median))
 
         return res
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for num in nums:
+            xor ^= num
+
+        return xor
