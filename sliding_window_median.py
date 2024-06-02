84b98d1fe7ebb3b970f1aad38d241e8507c2ebc6
Menghe Lu <lumenghe@gmail.com>
2024-06-02 16:40:48 +0200

    feat(sliding_window_median): sliding window median

--- sliding_window_median.py
+++ sliding_window_median.py
@@ -60,3 +60,33 @@ class SolutionSlow:
             ret.append((subb[k // 2] + subb[-1 - k // 2]) / 2)
 
         return ret
+
+
+from sortedcontainers import SortedList
+
+
+class Solution:
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        res = []
+        window = SortedList(nums[:k])
+
+        for i in range(k, len(nums)):
+            if k % 2 == 1:
+                median = window[k // 2]
+            else:
+                median = (window[k // 2] + window[k // 2 - 1]) / 2
+
+            res.append(float(median))
+
+            window.discard(nums[i - k])
+            window.add(nums[i])
+
+        # Process the last window
+        if k % 2 == 1:
+            median = window[k // 2]
+        else:
+            median = (window[k // 2] + window[k // 2 - 1]) / 2
+
+        res.append(float(median))
+
+        return res
