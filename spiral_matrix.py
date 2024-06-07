10ea546c0ecd0aefd934e749b41dd7496628eb1c
Menghe Lu <lumenghe@gmail.com>
2024-06-07 22:26:18 +0200

    feat(matrix): spiral matrix

--- spiral_matrix.py
+++ spiral_matrix.py
@@ -30,3 +30,45 @@ Constraints:
     -100 <= matrix[i][j] <= 100
 
 """
+from typing import List
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        res = []
+        while matrix:
+            print("res=",res)
+            res.extend(matrix.pop(0))
+            print(matrix)
+            print(*zip(*matrix))
+            matrix = [*zip(*matrix)][::-1]
+            print(matrix)
+        return res
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix:
+            return []
+
+        rows, cols = len(matrix), len(matrix[0])
+        top, bottom, left, right = 0, rows-1, 0, cols-1
+        result = []
+
+        while len(result) < rows * cols:
+            for i in range(left, right+1):
+                result.append(matrix[top][i])
+            top += 1
+
+            for i in range(top, bottom+1):
+                result.append(matrix[i][right])
+            right -= 1
+
+            if top <= bottom:
+                for i in range(right, left-1, -1):
+                    result.append(matrix[bottom][i])
+                bottom -= 1
+
+            if left<=right:
+                for i in range(bottom, top-1, -1):
+                    result.append(matrix[i][left])
+                left += 1
+
+        return result
