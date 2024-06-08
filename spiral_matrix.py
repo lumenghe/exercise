b1dda5a2d16566a27b062730c49a395d8881584b
Menghe Lu <lumenghe@gmail.com>
2024-06-08 07:17:58 +0200

    feat(rotate_image): rotate image

--- spiral_matrix.py
+++ spiral_matrix.py
@@ -30,12 +30,15 @@ Constraints:
     -100 <= matrix[i][j] <= 100
 
 """
+
 from typing import List
+
+
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         res = []
         while matrix:
-            print("res=",res)
+            print("res=", res)
             res.extend(matrix.pop(0))
             print(matrix)
             print(*zip(*matrix))
@@ -43,31 +46,32 @@ class Solution:
             print(matrix)
         return res
 
+
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         if not matrix:
             return []
 
         rows, cols = len(matrix), len(matrix[0])
-        top, bottom, left, right = 0, rows-1, 0, cols-1
+        top, bottom, left, right = 0, rows - 1, 0, cols - 1
         result = []
 
         while len(result) < rows * cols:
-            for i in range(left, right+1):
+            for i in range(left, right + 1):
                 result.append(matrix[top][i])
             top += 1
 
-            for i in range(top, bottom+1):
+            for i in range(top, bottom + 1):
                 result.append(matrix[i][right])
             right -= 1
 
             if top <= bottom:
-                for i in range(right, left-1, -1):
+                for i in range(right, left - 1, -1):
                     result.append(matrix[bottom][i])
                 bottom -= 1
 
-            if left<=right:
-                for i in range(bottom, top-1, -1):
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
                     result.append(matrix[i][left])
                 left += 1
 
