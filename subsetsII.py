8e41637299b0dee7e2ab04f0ec38f4d591ea3a25
Menghe Lu <lumenghe@gmail.com>
2024-06-09 07:33:02 +0200

    feat(backtrace): subsetsii

--- subsetsII.py
+++ subsetsII.py
@@ -30,3 +30,42 @@ Constraints:
     -10 <= nums[i] <= 10
 
 """
+
+from typing import List
+
+
+class SolutionWrong:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        def backtrack(start, path):
+            if path not in result:
+                result.append(path)
+            for i in range(start, len(nums)):
+                backtrack(i + 1, path + [nums[i]])
+
+        result = []
+        backtrack(0, [])
+        return result
+
+
+from collections import Counter
+
+
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        total = []
+
+        def backtrack(start, path):
+            if Counter(path) not in total:
+                total.append(Counter(path))
+                result.append(path)
+            for i in range(start, len(nums)):
+                backtrack(i + 1, path + [nums[i]])
+
+        result = []
+        backtrack(0, [])
+        return result
+
+
+nums = [4, 4, 4, 1, 4]
+
+expected = [[], [1], [1, 4], [1, 4, 4], [1, 4, 4, 4], [1, 4, 4, 4, 4], [4], [4, 4], [4, 4, 4], [4, 4, 4, 4]]
