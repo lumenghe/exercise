5ff925a1dc7ebb16af2234b58e041998bf2e6c05
Menghe Lu <lumenghe@gmail.com>
2024-06-14 22:54:25 +0200

    feat(window): container with most water

--- max_consecutive_ones.py
+++ max_consecutive_ones.py
@@ -34,6 +34,8 @@ Constraints:
 
 """
 
+from typing import List
+
 
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
@@ -45,4 +47,26 @@ class Solution:
                 if nums[l] == 0:
                     k += 1
                 l += 1
+            print(l, r)
         return r - l + 1
+
+
+s = Solution()
+nums = [
+    1,  # 0
+    1,  # 1
+    1,  # 2
+    1,  # 3
+    0,  # 4
+    0,  # 5
+    0,
+    1,
+    1,
+    1,
+    0,  # 10
+    0,
+    0,
+    1,
+    1,  # 14
+]
+print(s.longestOnes(nums, 2))
