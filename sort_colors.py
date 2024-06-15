fdd3088abe81b6149c4706e133ac51d6c816d4c4
Menghe Lu <lumenghe@gmail.com>
2024-06-15 07:57:01 +0200

    feat(window): sort colors

---
+++ sort_colors.py
@@ -0,0 +1,59 @@
+"""
+Sort Colors
+Solved
+Medium
+Topics
+Companies
+Hint
+
+Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+
+ 
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 300
+    nums[i] is either 0, 1, or 2.
+
+ 
+
+Follow up: Could you come up with a one-pass algorithm using only constant extra space?
+"""
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero, one = 0, 0
+        for num in nums:
+            if num == 0:
+                zero += 1
+            elif num == 1:
+                one += 1
+
+        for i in range(len(nums)):
+            if i < zero:
+                nums[i] = 0
+            elif zero <= i < zero + one:
+                nums[i] = 1
+            else:
+                nums[i] = 2
