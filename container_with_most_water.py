23654932a2a04fa9c6dfdc387c278ce3cd64dd61
Menghe Lu <lumenghe@gmail.com>
2024-06-15 07:37:09 +0200

    feat(window): container with most water

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -43,13 +43,13 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         left = 0
         right = len(height) - 1
-        maxArea = 0
+        max_area = 0
         while left < right:
             current = min(height[left], height[right]) * (right - left)
-            maxArea = max(maxArea, current)
+            max_area = max(current, max_area)
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
 
-        return maxArea
+        return max_area
