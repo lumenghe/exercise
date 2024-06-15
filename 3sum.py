1121f2eb343ecf3ff251e0e8572f37a1ea942659
Menghe Lu <lumenghe@gmail.com>
2024-06-15 17:18:49 +0200

    feat(points): 3 sum

--- 3sum.py
+++ 3sum.py
@@ -63,15 +63,16 @@ class Solution:
         if 0 < len(zero) < 3:
             for num in positive:
                 if -num in negative:
-                    if (0, num, -num) not in self.ret:
-                        self.ret.append((0, num, -num))
+                    if (-num, 0, num) not in self.ret:
+                        self.ret.append((-num, 0, num))
 
         def search(list1, list2):
             for i in range(len(list1)):
                 for j in range(i + 1, len(list1)):
                     if -(list1[i] + list1[j]) in list2:
-                        if (list1[i], list1[j], -(list1[i] + list1[j])) not in self.ret:
-                            self.ret.append((list1[i], list1[j], -(list1[i] + list1[j])))
+                        temp = list(sorted((list1[i], list1[j], -(list1[i] + list1[j]))))
+                        if temp not in self.ret:
+                            self.ret.append(temp)
 
         search(positive, negative)
         search(negative, positive)
@@ -79,109 +80,6 @@ class Solution:
 
 
 s = Solution()
-nums = [
-    34,
-    55,
-    79,
-    28,
-    46,
-    33,
-    2,
-    48,
-    31,
-    -3,
-    84,
-    71,
-    52,
-    -3,
-    93,
-    15,
-    21,
-    -43,
-    57,
-    -6,
-    86,
-    56,
-    94,
-    74,
-    83,
-    -14,
-    28,
-    -66,
-    46,
-    -49,
-    62,
-    -11,
-    43,
-    65,
-    77,
-    12,
-    47,
-    61,
-    26,
-    1,
-    13,
-    29,
-    55,
-    -82,
-    76,
-    26,
-    15,
-    -29,
-    36,
-    -29,
-    10,
-    -70,
-    69,
-    17,
-    49,
-]
+nums = [-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0]
 ret = s.threeSum(nums)
 print(len(ret))
-
-trueret = [
-    [-82, -11, 93],
-    [-82, 13, 69],
-    [-82, 17, 65],
-    [-82, 21, 61],
-    [-82, 26, 56],
-    [-82, 33, 49],
-    [-82, 34, 48],
-    [-82, 36, 46],
-    [-70, -14, 84],
-    [-70, -6, 76],
-    [-70, 1, 69],
-    [-70, 13, 57],
-    [-70, 15, 55],
-    [-70, 21, 49],
-    [-70, 34, 36],
-    [-66, -11, 77],
-    [-66, -3, 69],
-    [-66, 1, 65],
-    [-66, 10, 56],
-    [-66, 17, 49],
-    [-49, -6, 55],
-    [-49, -3, 52],
-    [-49, 1, 48],
-    [-49, 2, 47],
-    [-49, 13, 36],
-    [-49, 15, 34],
-    [-49, 21, 28],
-    [-43, -14, 57],
-    [-43, -6, 49],
-    [-43, -3, 46],
-    [-43, 10, 33],
-    [-43, 12, 31],
-    [-43, 15, 28],
-    [-43, 17, 26],
-    [-29, -14, 43],
-    [-29, 1, 28],
-    [-29, 12, 17],
-    [-14, -3, 17],
-    [-14, 1, 13],
-    [-14, 2, 12],
-    [-11, -6, 17],
-    [-11, 1, 10],
-    [-3, 1, 2],
-]
-print(len(trueret))
