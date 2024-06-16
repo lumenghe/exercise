87504956db2ae4cde7f3114b1d336eb8e46c0cf8
Menghe Lu <lumenghe@gmail.com>
2024-06-16 14:20:22 +0200

    feat(dfs): binary tree level order traversal

--- binary_tree_level_order_traversal.py
+++ binary_tree_level_order_traversal.py
@@ -32,3 +32,25 @@ Constraints:
     -1000 <= Node.val <= 1000
 
 """
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        self.result = {}
+
+        def dfs(node, level):
+            if node is None:
+                return
+            self.result.setdefault(level, [])
+            self.result[level].append(node.val)
+            dfs(node.left, level + 1)
+            dfs(node.right, level + 1)
+
+        dfs(root, 0)
+        return self.result.values()
