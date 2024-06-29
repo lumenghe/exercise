f97ee71c1aa838dc6b58ea029e11b465bd8c119a
Menghe Lu <lumenghe@gmail.com>
2024-06-29 15:54:45 +0200

    feat(binary_search): peak index in a mountain array

--- peak_index_in_a_mountain_array.py
+++ peak_index_in_a_mountain_array.py
@@ -40,14 +40,17 @@ Constraints:
     arr is guaranteed to be a mountain array.
 
 """
+
+
 class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         first = 0
-        last = len(arr) -1
+        last = len(arr) - 1
         while first < last:
-            mid = (first + last)//2
+            mid = (first + last) // 2
             if arr[mid] < arr[mid + 1]:
-                first = mid+1
+                first = mid + 1
             else:
                 last = mid
+        # return first or return last
         return last
