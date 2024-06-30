02a149e5ae75a10509fbdfae1efb12fa78ae7718
Menghe Lu <lumenghe@gmail.com>
2024-06-30 19:01:08 +0200

    feat(binary_search): search in rotated sorted array II

--- search_in_rotated_sorted_arrayII.py
+++ search_in_rotated_sorted_arrayII.py
@@ -49,17 +49,26 @@ class SolutionWrong:
 
         while left <= right:
             mid = (left + right) // 2
+            print(left, right, mid)
             if nums[mid] == target:
                 return True
             if nums[left] <= nums[mid]:
                 if nums[left] <= target < nums[mid]:
-                    right = mid
+                    right = mid - 1
                 else:
-                    left = mid
+                    left = mid + 1
             else:
                 if nums[mid] <= target <= nums[right]:
-                    left = mid
+                    left = mid + 1
                 else:
-                    right = mid
+                    right = mid - 1
 
-            return False
+        return False
+
+
+nums = [1, 0, 1, 1, 1]
+target = 0
+
+s = SolutionWrong()
+ret = s.search(nums, target)
+print(ret)
