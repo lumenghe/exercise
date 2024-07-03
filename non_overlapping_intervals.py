dc50d660d398c2cacc249ab2746072873f55a43b
Menghe Lu <lumenghe@gmail.com>
2024-07-03 22:14:49 +0200

    feat(intervals): non overlapping intervals

--- non_overlapping_intervals.py
+++ non_overlapping_intervals.py
@@ -35,3 +35,21 @@ Constraints:
     -5 * 104 <= starti < endi <= 5 * 104
 
 """
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+     
+        n = len(intervals)
+        intervals = sorted(intervals, key=lambda x: x[1])
+        print(intervals)
+        prev = 0
+        count = 1
+        for i in range(1, n):
+            if intervals[i][0] > intervals[prev][1]:
+                prev = i
+                count += 1
+
+        return n-count
+
+
+
+        
