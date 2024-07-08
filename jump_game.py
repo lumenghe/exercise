51ae9963af6d4fc209a9219a47982a739e386d3e
Menghe Lu <lumenghe@gmail.com>
2024-07-08 19:39:55 +0200

    feat(greedy); jump game

--- jump_game.py
+++ jump_game.py
@@ -31,3 +31,14 @@ Constraints:
     0 <= nums[i] <= 105
 
 """
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        gas = 0
+        for n in nums:
+            if gas < 0:
+                return False
+            elif n > gas:
+                gas = n
+            gas -= 1
+            
+        return True
