e41e3f481c961aa0e2470157d989427327aa5004
Menghe Lu <lumenghe@gmail.com>
2024-08-09 22:34:35 +0200

    feat(dp): maximum product subarray

--- maximum_product_subarray.py
+++ maximum_product_subarray.py
@@ -51,5 +51,38 @@ class Solution:
         return res
 
 
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        currentProduct = 1
+        maxProduct = float("-inf")
+
+        for num in nums:
+            currentProduct *= num
+
+            if currentProduct > maxProduct:
+                maxProduct = currentProduct
+
+            if currentProduct == 0:
+                currentProduct = num
+
+            if num > maxProduct:
+                maxProduct = num
+
+        currentProduct = 1
+        for num in nums[::-1]:
+            currentProduct *= num
+
+            if currentProduct > maxProduct:
+                maxProduct = currentProduct
+
+            if currentProduct == 0:
+                currentProduct = num
+
+            if num > maxProduct:
+                maxProduct = num
+
+        return maxProduct
+
+
 s = Solution()
 print(s.maxProduct([0, -3, 2, -4]))
