b46e9b2b3520d2dbae9f8e0767ad4e3472b15d15
Menghe Lu <lumenghe@gmail.com>
2024-08-11 11:47:44 +0200

    feat(dp): counting bits

--- counting_bits.py
+++ counting_bits.py
@@ -64,3 +64,18 @@ class Solution:
             # print(dp)
 
         return dp
+
+
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        if n == 0:
+            return [0]
+
+        dp = [0 for _ in range(n + 1)]
+        count_power_2 = 0
+        for i in range(1, n + 1):
+            if i == 2 ** (count_power_2 + 1):
+                count_power_2 += 1
+            dp[i] = dp[i - 2**count_power_2] + 1
+
+        return dp
