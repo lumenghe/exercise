d5f8b7fe5cfb327558d8956c9bf1184e04b0f957
Menghe Lu <lumenghe@gmail.com>
2024-08-11 17:11:42 +0200

    feat(dp): best time to buy and sell stock

--- all_possible_full_binary_trees.py
+++ all_possible_full_binary_trees.py
@@ -31,3 +31,28 @@ Constraints:
 
 
 """
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+
+        if n == 1:
+            return [TreeNode(0)]
+        if n % 2 == 0:
+            return []
+        result = []
+        for index in range(1, n, 2):
+            left = self.allPossibleFBT(index)
+            right = self.allPossibleFBT(n - index - 1)
+
+            for l in left:
+                for r in right:
+                    root = TreeNode(0, l, r)
+                    result.append(root)
+        return result
