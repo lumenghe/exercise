30fb1ecd60f0256f15e1dd03fa692324946e7c87
Menghe Lu <lumenghe@gmail.com>
2024-08-12 16:03:29 +0200

    feat(dp): palindrome partitioning

--- palindrome_partitioning.py
+++ palindrome_partitioning.py
@@ -53,3 +53,23 @@ class Solution:
         backtrack(0, [])
 
         return result
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+
+        def is_palindrome(sub):
+            return sub == sub[::-1]
+
+        def backtrack(start, path):
+            if start == len(s):
+                result.append(path)
+                return
+
+            for end in range(start, len(s)):
+                if is_palindrome(s[start : end + 1]):
+                    backtrack(end + 1, path + [s[start : end + 1]])
+
+        result = []
+        backtrack(0, [])
+        return result
